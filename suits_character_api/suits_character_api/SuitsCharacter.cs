e9f04f438120304c17f4dfef236426bcca5a902b
Nasrin Yoosuff <104313047+nasrinyoosuff123@users.noreply.github.com>
2023-01-11 20:26:41 -0800

    Add files via upload

---
+++ suits_character_api/suits_character_api/SuitsCharacter.cs
@@ -0,0 +1,12 @@
+﻿namespace suits_character_api
+{
+    public class SuitsCharacter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string JobTitle { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+    }
+}
